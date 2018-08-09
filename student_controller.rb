c453c90336fe9d13cba5b69ffe0b1fd2346c3cad
anne-other <anne.c.ince@gmail.com>
2018-08-09 13:03:09 +0100

    showing all students on one page

---
+++ student_controller.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+require_relative('./models/student.rb')
+also_reload('./models/*')
+
+#INDEX
+get '/students' do
+  @students = Student.all()
+  erb (:index)
+end
